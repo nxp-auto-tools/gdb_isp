bad98882155f7b5c8cef3d6964f6da518aa08f97
a.ivanushkin <anton.ivanushkin@nxp.com>
2017-10-09 18:39:58 +0300

    Scalar disassm skeleton

--- opcodes/apex-dis.c
+++ opcodes/apex-dis.c
@@ -1,14 +1,249 @@
 /*
  * Copyrights
  */
+#include <stdlib.h>
+#include <stdio.h>
 #include "sysdep.h"
-#include <ansidecl.h> //for ATTRIBUTE_UNUSED
 #include "dis-asm.h"
 #include "apex-opc.h"
+#include "apex-dis.h"
 #include "elf-bfd.h"
+#include "common/errors.h"
 
+typedef struct operand{
+	bfd_vma value;
+	operand_type type;
+}operand;
+extern const apex_opc_info_t const * const apex_APC_32b_scalar_opc_info;
+
+instruction_type get_instruction_type (bfd_vma instruction_word){ //read first two bit in instruction
+	instruction_word >>= 30;
+	switch (instruction_word){
+	case 0:
+		return scalar_instruction_type;
+	case 1:
+		return vector_instruction_type;
+	case 2:
+		return combined_instruction_type;
+	case 3:
+		return scalar64_instruction_type;
+	default:
+		//warning("Wrong instruction type");
+		return wrong_insruction_type;
+	}
+}
+apex_opc_info_t finde_in_table (apex_opc_info_t* table, bfd_vma data){ // brute force yet
+	for(;table->name;table++)
+		if ((data & ~table.op_pos & ~table.non_read_pos) == table.opcode)
+			return table;
+	return NULL;
+}
+
+unsigned int extract_operands (apex_opc_info_t operation,operand* operands,bfd_vma data){
+
+	bfd_vma op_mask[5];
+	unsigned char positions_to_shift[5];
+
+	memset (op_mask, 0, 5*sizeof(bfd_vma));
+	memset (positions_to_shift, 0, 5);
+
+	switch(operation.op_pos){ // compare operands mask
+
+	case first_scalar_operands_mask_type:
+		/*OPERAND_FIRST|OPERAND_SECOND|OPERAND_C*/
+		op_mask[0]=OPERAND_FIRST;
+		op_mask[1]=OPERAND_SECOND;
+		op_mask[2]=OPERAND_C;
+		positions_to_shift[0]=20;
+		positions_to_shift[1]=15;
+		positions_to_shift[2]=0;
+		break;
+
+	case second_scalar_operands_mask_type:
+		/*OPERAND_FIRST|OPERAND_SECOND|OPERAND_THIRD*/
+		op_mask[0]=OPERAND_FIRST;
+		op_mask[1]=OPERAND_SECOND;
+		op_mask[2]=OPERAND_THIRD;
+		positions_to_shift[0]=20;
+		positions_to_shift[1]=15;
+		positions_to_shift[2]=10;
+		break;
+
+	case third_scalar_operands_mask_type:
+		/*SHIFT_LEFT(OPERAND_SECOND,1)|OPERAND_IMM*/
+		op_mask[0]=SHIFT_LEFT(OPERAND_SECOND,1);
+		op_mask[1]=OPERAND_IMM;
+		positions_to_shift[0]=16;
+		positions_to_shift[1]=0;
+		break;
+
+	case fourth_scalar_operands_mask_type:
+		/*OPERAND_FIRST|OPERAND_SECOND|OPERAND_THIRD|OPERAND_FOURTH*/
+		op_mask[0]=OPERAND_FIRST;
+		op_mask[1]=OPERAND_SECOND;
+		op_mask[2]=OPERAND_THIRD;
+		op_mask[3]=OPERAND_FOURTH;
+		positions_to_shift[0]=20;
+		positions_to_shift[1]=15;
+		positions_to_shift[2]=10;
+		positions_to_shift[3]=5;
+		break;
+
+	case fifth_scalar_operands_mask_type:
+		/*OPERAND_FIRST|OPERAND_IMM*/
+		op_mask[0]=OPERAND_FIRST;
+		op_mask[1]=OPERAND_IMM;
+		positions_to_shift[0]=20;
+		positions_to_shift[1]=0;
+		break;
+
+	case sixth_scalar_operands_mask_type:
+		/*OPERAND_FIRST|OPERAND_SECOND*/
+		op_mask[0]=OPERAND_FIRST;
+		op_mask[1]=OPERAND_SECOND;
+		positions_to_shift[0]=20;
+		positions_to_shift[1]=15;
+		break;
+
+	case seventh_scalar_operands_mask_type:
+		/*OPERAND_SECOND|OPERAND_THIRD*/
+		op_mask[0]=OPERAND_SECOND;
+		op_mask[1]=OPERAND_THIRD;
+		positions_to_shift[0]=15;
+		positions_to_shift[1]=10;
+		break;
+
+	case eighth_scalar_operands_mask_type:
+		/*OPERAND_SECOND|OPERAND_THIRD_EXT_1*/
+		op_mask[0]=OPERAND_SECOND;
+		op_mask[1]=OPERAND_THIRD_EXT_1;
+		positions_to_shift[0]=15;
+		positions_to_shift[1]=9;
+		break;
+
+	case ninth_scalar_operands_mask_type:
+		/*OPERAND_LARGE_IMM*/
+		op_mask[0]=OPERAND_LARGE_IMM;
+		positions_to_shift[0]=0;
+		break;
+
+	case tenth_scalar_operands_mask_type:
+		/*OPERAND_FIRST*/
+		op_mask[0]=OPERAND_FIRST;
+		positions_to_shift[0]=20;
+		break;
+
+	case eleventh_scalar_operands_mask_type:
+		/*OPERAND_SECOND*/
+		op_mask[0]=OPERAND_SECOND;
+		positions_to_shift[0]=15;
+		break;
+
+	case twelfth_scalar_operands_mask_type:
+		/*OPERAND_I1|OPERAND_I2*/
+		op_mask[0]=OPERAND_I1;
+		op_mask[1]=OPERAND_I2;
+		positions_to_shift[0]=13;
+		positions_to_shift[1]=0;
+		break;
+
+	case thirteenth_scalar_operands_mask_type:
+		/*OPERAND_IMM*/
+		op_mask[0]=OPERAND_IMM;
+		positions_to_shift[0]=0;
+		break;
+
+	case no_operands:
+		break;
+	default:
+		operands=NULL;
+		return -1;
+	}
+	operands = malloc(sizeof(operand)*operation.num_of_operands);
+	if (operands==NULL){
+		return -1;
+	}
+	for (unsigned char i=0; i<operation.num_of_operands;i++){
+		operands[i].type = operation.op_type[i];
+		operands[i].value = SHIFT_RIGHT((data & op_mask[i]), positions_to_shift[i]);
+	}
+	return 0;
+
+}
+int compose_disasmed_string (apex_opc_info_t instruction,operand* operands,const char* string){
+
+	strcat(string, instruction->name);
+	for (int i=0;i<instruction.num_of_operands;i++){
+		switch(operands[i].type){
+		case reg_t:
+			strcat(string," r");
+			break;
+		case imm_t:
+			strcat(string," #");
+			break;
+		}
+	sprintf(string,"%d",operands[i].value);
+	}
+	return strlen(string);
+}
 int
-print_insn_apex(bfd_vma pc  ATTRIBUTE_UNUSED, disassemble_info *info  ATTRIBUTE_UNUSED){
+print_insn_apex(bfd_vma pc, disassemble_info *info){
 
+	bfd_byte instr_low_bytes [word_instruction_length];
+	bfd_byte instr_high_bytes [word_instruction_length];
+	apex_opc_info_t* opcode_table;
+	apex_opc_info_t current_instruction;
+	operand* operands; //dynamic!!! to free!
+	const char* instr_string_shape[64];
+
+    // read instruction-word at address pointed by "pc"
+    int status = (*info->read_memory_func) (pc, instr_low_bytes,
+    									word_instruction_length, info);
+    if (status != 0)
+    {
+      (*info->memory_error_func) (status, pc, info);
+      return -1;
+    }
+
+    // read next instruction-word at address pointed by "pc+1"
+    /*status = (*info->read_memory_func) (pc+1, instr_low_bytes,
+    									word_instruction_length, info);
+    if (status != 0)
+    {
+      (*info->memory_error_func) (status, pc, info);
+      return -1;
+    }*/
+
+    bfd_vma data = bfd_get_bits (instr_low_bytes, word_instruction_length * 8, info->display_endian == BFD_ENDIAN_LITTLE);
+    switch (get_instruction_type(data)){
+    case scalar_instruction_type:
+    	opcode_table = apex_APC_32b_scalar_opc_info;
+    	break;
+    case vector_instruction_type:
+    	break;
+    case combined_instruction_type:
+    	break;
+    case scalar64_instruction_type:
+    	break;
+    default:
+    	error ("Wrong instruction type");
+    	break;
+
+    }
+    current_instruction = finde_in_table(opcode_table,data);
+    if (current_instruction == NULL){
+    	error("can't find in table this instruction!\n");
+    	return -1;
+    }
+
+    if(extract_operands(current_instruction,operands,data)<0){
+    	error("Can't extract operands!\n");
+    	return -1;
+    }
+    if(compose_disasmed_string(current_instruction,operands,instr_string_shape)<=0){
+    	error("Error while composing string!\n");
+    	return -1;
+    }
+    free(operands);
 	return 0;
 }
--- opcodes/apex-opc.h
+++ opcodes/apex-opc.h
@@ -5,18 +5,10 @@
 #ifndef OPCODE_APEX_H
 #define OPCODE_APEX_H
 
-typedef struct apex_opc_info_t
-{
-  const char *name;
-  unsigned char instr_t; // instruction type (Scalar(0x0), Vector(0x1), Combined(0x2))
-  unsigned long opcode;
-  unsigned long operands; //operands positions
-} apex_opc_info_t;
+#define SHIFT_LEFT(v, p) ((v)<<(p)) //v - value; p - number of positions to shift
+#define SHIFT_RIGHT(v, p) ((v)>>(p))
 
-/* Bit-masks for instruction members separation use "&&" */
-/*#define INST_TYPE			0xC0000000;
-#define INST_SHORT_OPCODE	0x3E000000;
-#define INST_LONG_OPCODE	0x3E0000FF;*/
+#define NONE_OPERAND 0x00000000U
 
 #define OPERAND_FIRST			0x01F00000U //[20:24] bits
 #define OPERAND_SECOND			0x000F8000U //[15:19] bits
@@ -29,10 +21,56 @@ typedef struct apex_opc_info_t
 #define OPERAND_I1				0x01FFE000U //[13:24] bits
 #define OPERAND_I2				0x00001FFFU	// [0:12] bits
 
-#define SHIFT_LEFT(v, p) ((v)<<(p)) //v - value; p - number of positions to shift
-#define SHIFT_RIGHT(v, p) ((v)>>(p))
+#define OPERAND_s0				0x00000C00U //[10:11] bits
+#define OPERAND_i0				0x01C00000U //[22:24] bits
+#define OPERAND_i0_shftd_right	0x00380000U //[19:21] bits
+#define OPERAND_d0				0x01800000U //[22:23] bits
 
+enum scalar_operands_mask_types{
+	 first_scalar_operands_mask_type = OPERAND_FIRST|OPERAND_SECOND|OPERAND_C,
+	second_scalar_operands_mask_type = OPERAND_FIRST|OPERAND_SECOND|OPERAND_THIRD,
+	 third_scalar_operands_mask_type = SHIFT_LEFT(OPERAND_SECOND,1)|OPERAND_IMM,
+	fourth_scalar_operands_mask_type = OPERAND_FIRST|OPERAND_SECOND|OPERAND_THIRD|OPERAND_FOURTH,
+	 fifth_scalar_operands_mask_type = OPERAND_FIRST|OPERAND_IMM,
+	 sixth_scalar_operands_mask_type = OPERAND_FIRST|OPERAND_SECOND,
+   seventh_scalar_operands_mask_type = OPERAND_SECOND|OPERAND_THIRD,
+    eighth_scalar_operands_mask_type = OPERAND_SECOND|OPERAND_THIRD_EXT_1,
+	 ninth_scalar_operands_mask_type = OPERAND_LARGE_IMM,
+	 tenth_scalar_operands_mask_type = OPERAND_FIRST,
+  eleventh_scalar_operands_mask_type = OPERAND_SECOND,
+   twelfth_scalar_operands_mask_type = OPERAND_I1|OPERAND_I2,
+thirteenth_scalar_operands_mask_type = OPERAND_IMM,
+					no_operands = NONE_OPERAND
+}scalar_operands_mask_types;
 
+enum vector_operands_mask_types{
+
+}vector_operands_mask_types;
+
+enum combined_operands_mask_types{
+
+}combined_operands_mask_types;
+
+enum scalar_64_operands_mask_types{
+
+}scalar_64_operands_mask_types;
+
+typedef enum operand_type{
+	reg_t,
+	imm_t,
+	gap
+}operand_type;
+
+typedef struct apex_opc_info_t
+{
+  const char *name;
+  unsigned long long opcode;
+  unsigned char num_of_operands;
+  operand_type op_type[5]={gap,gap,gap,gap,gap};
+  unsigned long long op_pos; //operands positions
+  unsigned long long non_read_pos; //positions of instr, that not reads by.
+
+} apex_opc_info_t;
 
 
 #endif // OPCODE_APEX_H
