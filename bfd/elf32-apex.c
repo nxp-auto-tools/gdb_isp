ecb540c1f17beaf2d288973eeccadefccf5060b6
Anton Ivanushkin-nxf37446 <anton.ivanushkin@nxp.com>
2017-12-28 19:49:33 +0300

    Not related things deleted

--- bfd/elf32-apex.c
+++ bfd/elf32-apex.c
@@ -7,7 +7,6 @@
 #include "libbfd.h"
 #include "elf-bfd.h"
 #include "elf/apex.h"
-
 //#define TARGET_LITTLE_SYM apex_elf32_def_vec
 //#define TARGET_LITTLE_NAME "elf32-apex"
 #ifndef TARGET_BIG_SYM
@@ -19,19 +18,19 @@
 #define ELF_COMMONPAGESIZE 0x2000
 #define ELF_MACHINE_CODE	EM_APEX
 
+
+
 static reloc_howto_type *
-apex_reloc_type_lookup (bfd *abfd ATTRIBUTE_UNUSED,
-				 bfd_reloc_code_real_type code ATTRIBUTE_UNUSED){
+bfd_elf32_bfd_reloc_type_lookup (bfd *abfd ATTRIBUTE_UNUSED,
+				 bfd_reloc_code_real_type code ATTRIBUTE_UNUSED)
+{
+
   return NULL;
 }
-
 static reloc_howto_type *
-apex_reloc_name_lookup (bfd *abfd ATTRIBUTE_UNUSED,
-				 const char *r_name ATTRIBUTE_UNUSED){
+bfd_elf32_bfd_reloc_name_lookup (bfd *abfd ATTRIBUTE_UNUSED,
+				 const char *r_name ATTRIBUTE_UNUSED)
+{
     return NULL;
 }
-
-#define bfd_elf32_bfd_reloc_type_lookup		apex_reloc_type_lookup
-#define bfd_elf32_bfd_reloc_name_lookup		apex_reloc_name_lookup
-
 #include "elf32-target.h"
